bc5e45e99dc60738db6806ba3361d5bae639d623
usernamefanta <143836045+usernamefanta@users.noreply.github.com>
2023-10-14 17:50:19 +0300

    Create ProductList.vue

---
+++ frontend/src/components/ProductList.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <h2>Products</h2>
+        <ul>
+            <li v-for="product in products" :key="product._id">
+                {{ product.name }} - ${{ product.price }}
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            products: [],
+        };
+    },
+    mounted() {
+        this.fetchProducts();
+    },
+    methods: {
+        async fetchProducts() {
+            try {
+                const response = await this.$axios.get('/api/products');
+                this.products = response.data;
+            } catch (error) {
+                console.error('Error fetching products:', error);
+            }
+        },
+    },
+};
+</script>
