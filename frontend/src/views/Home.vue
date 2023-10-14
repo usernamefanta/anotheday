9fc35c6d443c02f68d0c54fe0ef91da849cb9b37
usernamefanta <143836045+usernamefanta@users.noreply.github.com>
2023-10-14 17:49:32 +0300

    Create Home.vue

---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1>Product Listing</h1>
+        <ProductList />
+    </div>
+</template>
+
+<script>
+import ProductList from '../components/ProductList.vue';
+
+export default {
+    components: {
+        ProductList,
+    },
+};
+</script>
